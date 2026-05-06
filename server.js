147de5fdc9d30e12367009261426b32d3d2f139a
iqbalnrflah <iqbalnurfalah2501@gmail.com>
2026-05-06 14:58:21 +0700

    setting

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 const express = require("express");
-const app = express();
+const midtransClient = require("midtrans-client");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("SERVER OK");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const snap = new midtransClient.Snap({
+  isProduction: false, // sandbox dulu
+  serverKey: "Mid-server-aH6j_Xq7s4fwsInRCOtsMiQV",
 });
 
-const PORT = process.env.PORT || 3000;
+app.post("/bayar", async (req, res) => {
+  try {
+    const { nama, amount } = req.body;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("SERVER RUNNING", PORT);
+    const parameter = {
+      transaction_details: {
+        order_id: "INV-" + Date.now(),
+        gross_amount: amount,
+      },
+      customer_details: {
+        first_name: nama,
+      },
+    };
+
+    const transaction = await snap.createTransaction(parameter);
+
+    res.json({
+      url: transaction.redirect_url,
+    });
+
+  } catch (err) {
+    console.log("ERROR:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server jalan di http://192.168.1.10:3000");
 });
